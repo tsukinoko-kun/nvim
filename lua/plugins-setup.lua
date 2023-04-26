1ad3bce835c84591731ac0251bbbda5bf5b9f91d
Frank-Mayer <git@frank-mayer.io>
2023-04-26 19:39:06 +0200

    git worktree

--- lua/plugins-setup.lua
+++ lua/plugins-setup.lua
@@ -203,6 +203,7 @@ return packer.startup(function(use)
     use("lewis6991/gitsigns.nvim") -- show line modifications on left hand side
     use("tpope/vim-fugitive") -- git commands in vim
     use("kdheepak/lazygit.nvim") -- lazygit in vim
+    use("ThePrimeagen/git-worktree.nvim") -- git worktree integration
 
     use({
         "github/copilot.vim",
