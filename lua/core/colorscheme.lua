c07c7434ed3b7decbd9cb42bb3c66b668bfbf1c4
Frank Mayer <git@frank-mayer.io>
2023-06-06 19:52:13 +0200

    6 to 20 light mode

--- lua/core/colorscheme.lua
+++ lua/core/colorscheme.lua
@@ -1,5 +1,5 @@
 local hour = tonumber(os.date("%H"))
-if hour >= 6 and hour < 18 then
+if hour >= 6 and hour < 20 then
     pcall(vim.cmd.colorscheme, "catppuccin-latte")
 else
     pcall(vim.cmd.colorscheme, "catppuccin-mocha")
