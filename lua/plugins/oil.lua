316e50e1ea1db0fb9c76662573396461a69903f0
Frank Mayer <git@frank-mayer.io>
2023-07-02 16:39:38 +0200

    no permissions in explorer

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -5,7 +5,7 @@ return {
         -- See :help oil-columns
         columns = {
             "icon",
-            "permissions",
+            -- "permissions",
             -- "size",
             -- "mtime",
         },
