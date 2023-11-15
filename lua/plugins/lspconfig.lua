4aacd65ccd65101a31f752ff447ea79fd2632c24
Frank Mayer <gpg@frankmayer.dev>
2023-11-15 23:29:46 +0100

    added selene lua types

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -210,8 +210,9 @@ return {
                     workspace = {
                         -- make language server aware of runtime files
                         library = {
-                            [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                            [vim.fn.stdpath("config") .. "/lua"] = true,
+                            vim.fn.expand("$VIMRUNTIME/lua"),
+                            vim.fn.stdpath("config") .. "/lua",
+                            vim.fn.expand("$XDG_CONFIG_HOME/selene/lib"),
                         },
                     },
                 },
