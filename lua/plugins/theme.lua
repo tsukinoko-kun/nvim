8b101c80797686024ea0c3dfbe3ead1ffc4ed888
Frank Mayer <gh@frankmayer.dev>
2024-01-01 15:29:25 +0100

    catppuccin

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,11 +1,19 @@
 return {
-    "jesseleite/nvim-noirbuddy",
-    dependencies = {
-        { "tjdevries/colorbuddy.nvim" },
-    },
-    lazy = false,
+    "catppuccin/nvim",
+    name = "catppuccin",
     priority = 1000,
     opts = {
-        -- All of your `setup(opts)` will go here
+        flavour = "mocha",
+        show_end_of_buffer = true,
+        integrations = {
+            cmp = true,
+            gitsigns = true,
+            nvimtree = true,
+            telescope = true,
+            notify = false,
+            mini = false,
+            harpoon = true,
+            mason = true,
+        },
     },
 }
