faa0174769303bc204ae22b162ead32b1437d5fe
Frank Mayer <gh@frankmayer.dev>
2024-01-01 13:32:03 +0100

    better lualine file name

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,47 +2,52 @@ return {
     "nvim-lualine/lualine.nvim",
     dependencies = {
         "arkav/lualine-lsp-progress",
+        "catppuccin/nvim",
     },
-    config = function()
-        require("lualine").setup({
-            sections = {
-                lualine_a = { "mode" },
-                lualine_b = { "branch", "diff", "diagnostics" },
-                lualine_c = {
-                    function()
-                        -- relative file path from project root
-                        local filename = vim.fn.expand("%")
-                        if vim.bo.readonly then
-                            filename = filename .. " [RO]"
-                        end
-                        if vim.bo.modified then
-                            filename = filename .. " [+]"
-                        end
-                        return filename
-                    end,
-                    "lsp_progress",
+    opts = {
+        sections = {
+            lualine_a = { "mode" },
+            lualine_b = { "branch", "diagnostics" },
+            lualine_c = { {
+                'filename',
+                file_status = true,        -- Displays file status (readonly status, modified status)
+                newfile_status = true,     -- Display new file status (new file means no write after created)
+                path = 1,
+                -- 0: Just the filename
+                -- 1: Relative path
+                -- 2: Absolute path
+                -- 3: Absolute path, with tilde as the home directory
+                -- 4: Filename and parent dir, with tilde as the home directory
+
+                shorting_target = 40,     -- Shortens path to leave 40 spaces in the window
+                -- for other components. (terrible name, any suggestions?)
+                symbols = {
+                    modified = '󰏫', -- Text to show when the file is modified.
+                    readonly = ' ', -- Text to show when the file is non-modifiable or readonly.
+                    unnamed = '󰜣 ', -- Text to show for unnamed buffers.
+                    newfile = ' ', -- Text to show for newly created file before first write
+                }
+            } },
+            lualine_x = {},
+            lualine_y = {
+                "encoding",
+                "fileformat",
+                {
+                    "filetype",
+                    colored = false,
+                    icon_only = false,
                 },
-                lualine_x = {},
-                lualine_y = {
-                    "encoding",
-                    "fileformat",
-                    {
-                        "filetype",
-                        colored = false,
-                        icon_only = false,
-                    },
-                },
-                lualine_z = { "location" },
             },
-            options = {
-                theme = "catppuccin",
-                component_separators = { left = "│", right = "│" },
-                section_separators = { left = "", right = "" },
-            },
-            tabline = {},
-            winbar = {},
-            inactive_winbar = {},
-            extensions = {},
-        })
-    end,
+            lualine_z = { "location" },
+        },
+        options = {
+            theme = "catppuccin",
+            component_separators = { left = "", right = "" },
+            section_separators = { left = "", right = "" },
+        },
+        tabline = {},
+        winbar = {},
+        inactive_winbar = {},
+        extensions = {},
+    },
 }
