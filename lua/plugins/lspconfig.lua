3128d303cf93f1737698d3f4fe29cc17b55cd842
Frank Mayer <gh@frankmayer.dev>
2024-08-29 02:16:48 +0200

    wakatime

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -335,7 +335,7 @@ return {
 
             lspconfig.typst_lsp.setup({
                 settings = {
-                    exportPdf = "onType", -- Choose onType, onSave or never.
+                    exportPdf = "never", -- Choose onType, onSave or never.
                     -- serverPath = "" -- Normally, there is no need to uncomment it.
                 },
             })
